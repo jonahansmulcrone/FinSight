cb5b22f2be0bfaff82354832eaec2ba2eeab706a
ezrahuffman <joshooha@gmail.com>
2025-03-30 20:08:38 -0700

    Add company names to the watchlist

--- server/main.py
+++ server/main.py
@@ -9,12 +9,14 @@ from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
 import asyncio
+from polygon import RESTClient
 
 load_dotenv()
 
 app = FastAPI()
 pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
 r = redis.Redis(connection_pool=pool)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,10 +39,9 @@ async def get_watchlist(
     multiplier: int = 1,
     limit: int = 1000
 ):
-    
     cache = r.get('user_1')
 
-    if cache:
+    if False:
         print('cache hit')
         cached_data = json.loads(cache)
         return WatchlistDataResponse(
@@ -54,6 +55,7 @@ async def get_watchlist(
             raise HTTPException(status_code=500, detail="API key not configured in .env file")
         
         ticker_list = [ticker.strip().upper() for ticker in tickers.split(",")]
+        client = RESTClient(api_key)
         
         if not start_date:
             # Default to previous trading day
@@ -81,6 +83,7 @@ async def get_watchlist(
 
                     response.raise_for_status()
                     data = response.json()
+                    #print(data)
                     
                     if "results" not in data or not data["results"]:
                         return {
@@ -122,10 +125,35 @@ async def get_watchlist(
                     "error": str(e)
                 }
         
+        async def get_ticker_info(ticker):
+            try:
+                details = client.get_ticker_details(ticker,)
+                return {
+                    "name": details.name,
+                }
+            
+            except httpx.HTTPStatusError as e:
+                print(e)
+                return {
+                    "error": f"HTTP error: {e.response.status_code} - {e.response.text}"
+                }
+            except Exception as e:
+                print(e)
+                return {
+                    
+                    "error": str(e)
+                }
         # Process each ticker concurrently
+        ticker_info_tasks = [get_ticker_info(ticker) for ticker in ticker_list]
+        ticker_info_results = await asyncio.gather(*ticker_info_tasks)
         tasks = [get_ticker_data(ticker) for ticker in ticker_list]
         results_list = await asyncio.gather(*tasks) # gather() concurrently runs multiple coroutines.
         
+        #TODO: there is almost certainly a better way to do this, just doing this to for testing/proto
+        if len(ticker_info_results) > 0 and "name" in ticker_info_results[0]:
+            for i in range(len(results_list)):
+                results_list[i]["name"] = ticker_info_results[i]["name"]
+
         # Map results to tickers
         results = {ticker: result for ticker, result in zip(ticker_list, results_list)}
 
