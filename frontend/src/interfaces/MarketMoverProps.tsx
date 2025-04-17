51dc19bab6412c2dd58d7210c8a79ef2b7bf21d7
ezrahuffman <joshooha@gmail.com>
2025-04-16 22:58:00 -0700

    New dropdown component, types, interface, and error handling on backend

---
+++ frontend/src/interfaces/MarketMoverProps.tsx
@@ -0,0 +1,7 @@
+import MarketMoverData from '../types/MarketMoverData'
+
+interface MarketMoverProps {
+    data: MarketMoverData,
+}
+
+export default MarketMoverProps;
---
+++ frontend/src/types/MarketMoverData.tsx
@@ -0,0 +1,9 @@
+type MarketMoverData = {
+    ticker: string,
+    price: string,
+    change_amount: string,
+    change_percentage: string,
+    volume: string
+};
+
+export default MarketMoverData
