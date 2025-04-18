77145cdb46a697de6bd92e8746afdd06f579b0ca
jonahansmulcrone <126010204+jonahansmulcrone@users.noreply.github.com>
2025-04-18 15:17:46 -0700

    compelted trading info component

--- frontend/src/components/dashboard/sections/overview/TradingInfo.tsx
+++ frontend/src/components/dashboard/sections/overview/TradingInfo.tsx
@@ -31,11 +31,13 @@ const TradingInfo: React.FC = () => {
                     Object.entries(tradingInfo).map((item) =>
                         <div className='flex flex-col items-start justify-start gap-1.5'>
                             <div className='text-sm flex flex-row justify-between items-center w-full'>
+                                {/* Info Type */}
                                 <div>
-                                    Something
+                                    {item[0]}
                                 </div>
+                                {/* Info Value */}
                                 <div className='font-semibold'>
-                                    {item[1]}
+                                    ${item[1]}
                                 </div>
                             </div>
                             <div className='w-full h-[1px] bg-gray-200' />
